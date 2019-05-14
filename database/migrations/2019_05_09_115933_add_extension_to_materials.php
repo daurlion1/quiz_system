f998b4918ab881ea5eb164370c21dfc6f0cbfba9
daur <daur-lion@mail.ru>
2019-05-14 12:19:00 +0600

    миграции

---
+++ database/migrations/2019_05_09_115933_add_extension_to_materials.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddExtensionToMaterials extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subjects', function (Blueprint $table) {
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
