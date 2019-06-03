be9e2f3cbf67c3f8845e117e89cabe96aa47dc71
spider-unknown <beka_97.50@mail.ru>
2019-06-03 08:39:36 +0600

    Правки теста

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,23 +14,23 @@
                         </div>
                     @endif
 
-                    You are logged in !
+                    Welcome to admin page!
 
-                        <div class="card-deck">
-                            @foreach($results as $result)
-                            <div class="card">
-                                <div class="card-heading text-center  bg-info">
+                        {{--<div class="card-deck">--}}
+                            {{--@foreach($results as $result)--}}
+                            {{--<div class="card">--}}
+                                {{--<div class="card-heading text-center  bg-info">--}}
 
-                                    <h3 class="text-center"> {{$result->quiz->title}}</h3>
+                                    {{--<h3 class="text-center"> {{$result->quiz->title}}</h3>--}}
 
-                                </div>
-                                <div class="card-body text-center">
-                                    <h1 class="text-center">{{$result->result}}</h1>
-                                    <h1 class="text-center">{{$result->student->user->name}}</h1>
-                                </div>
-                            </div>
-                                @endforeach
-                        </div>
+                                {{--</div>--}}
+                                {{--<div class="card-body text-center">--}}
+                                    {{--<h1 class="text-center">{{$result->result}}</h1>--}}
+                                    {{--<h1 class="text-center">{{$result->student->user->name}}</h1>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                                {{--@endforeach--}}
+                        {{--</div>--}}
                 </div>
             </div>
         </div>
